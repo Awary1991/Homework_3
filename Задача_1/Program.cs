cca7f879caac3b93ae9a07f5e7a0f3ee3188312e
Awary <Nolofinve1991@gmail.com>
2023-04-07 08:49:28 +0500

    дОмашняя работа по третьему семинару

---
+++ Задача_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+if (num[0] == num[4] & num[1] == num[3])
+    {
+        Console.WriteLine("Это полиндром");
+    }
+else 
+    Console.WriteLine("не полиндром");
